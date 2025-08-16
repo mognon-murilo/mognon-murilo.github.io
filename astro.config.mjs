fc895e7e5da063d1b517a40ad88cf8acb2966ed5
Murilo Mognon <mognonmmurilo@gmail.com>
2025-08-16 20:23:56 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,2 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: node({
-    mode: 'static',
-  }),
-  // A base da URL para seu site
-  base: '/',
-  // O diretório de saída para o build
-  outDir: './dist',
-});
+export default defineConfig({});
